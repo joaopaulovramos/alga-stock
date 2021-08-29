a6c197c5349492ec0ebbcaee210fba386984108f
jpramos <joaopaulovieiraramos@gmail.com>
2021-08-29 13:59:10 -0300

    add hook userefect

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './TestComponent.css'
 
 function TestComponent(props: { name: string }) {
   const [age, setAge] = useState(35)
 
+  useEffect(() =>{
+    console.log('Componente foi Criado')
+  }, [])
+  
+  useEffect(() =>{
+    console.log('Age Updated')
+  }, [age])
+
   return <div className="TestComponent">
     { props.name }, { age }
     <button onClick={() => {
       setAge(age + 1)
-      console.log(age)
     }}>+
     </button>
     </div>
