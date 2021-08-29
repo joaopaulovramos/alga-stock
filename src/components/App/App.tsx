8ddbeb578ae3535255d9716bd1e0826a6573556e
jpramos <joaopaulovieiraramos@gmail.com>
2021-08-29 14:01:50 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './img/logo.svg';
 import './App.css';
-import TestComponent from '../TestComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Joao" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
