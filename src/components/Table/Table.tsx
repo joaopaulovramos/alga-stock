1e2217589bec76aa0137a6439a0e9e5e7117c5b0
jpramos <joaopaulovieiraramos@gmail.com>
2021-08-30 16:35:20 -0300

    add scss in Table

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,26 +1,26 @@
 import React from "react"
-// import './Table.scss'
+import './Table.scss'
 
 
 const Table = () => {
-  return <table className="table">
+  return <table className="table AppTable">
     <thead>
       <tr>
         <th>Products</th>
         <th>Price</th>
-        <th>Stock</th>
+        <th className="right">Stock</th>
       </tr>
     </thead>
     <tbody>
       <tr>
       <td>Cookie</td>
       <td>$1.25</td>
-      <td>23</td>
+      <td className="right">23</td>
       </tr>
       <tr>
       <td>Milk</td>
       <td>$0.99</td>
-      <td>52</td>
+      <td className="right">52</td>
       </tr>
     </tbody>
   </table>
