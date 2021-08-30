eaa27ff8afe740aa45fff44ed4129b44b21b1669
jpramos <joaopaulovieiraramos@gmail.com>
2021-08-30 10:20:39 -0300

    add component table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,16 @@
-import React, { useState } from 'react';
-import Button from '../shared/Button';
+import React from 'react';
 import Header from '../Header';
 import Container from '../shared/Container';
 import './App.css';
-import Input from '../Input';
-
-function TestComponent() {
-  return <i className="fas fa-search-plus"></i>
-}
-
 
 function App() {
-  const [street, setStreet] = useState('')
+  
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Button
-          onClick={() => window.alert('Voce clicou no botão')}
-          appendIcon={<TestComponent />}  
-        >
-          Search
-        </Button>
-      </Container>
-      <Input 
-      label="Street"
-      placeholder="E.g: Avenida Castelo Branco"
-      value={street}
-      onChange={(event) => setStreet(event.target.value)}
-      />
+
+      </Container>  
     </div>
   );
 }
