b9a4474c5a82e969ae6c4ccb8c376fe41a82b9b3
jpramos <joaopaulovieiraramos@gmail.com>
2021-08-30 10:07:14 -0300

    add input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../shared/Button';
 import Header from '../Header';
+import Container from '../shared/Container';
 import './App.css';
+import Input from '../Input';
+
+function TestComponent() {
+  return <i className="fas fa-search-plus"></i>
+}
+
 
 function App() {
+  const [street, setStreet] = useState('')
   return (
     <div className="App">
       <Header title="AlgaStock" />
+      <Container>
+        <Button
+          onClick={() => window.alert('Voce clicou no botão')}
+          appendIcon={<TestComponent />}  
+        >
+          Search
+        </Button>
+      </Container>
+      <Input 
+      label="Street"
+      placeholder="E.g: Avenida Castelo Branco"
+      value={street}
+      onChange={(event) => setStreet(event.target.value)}
+      />
     </div>
   );
 }
