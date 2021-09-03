234efa545681864d2e74b20325be3428fc908361
jpramos <joaopaulovieiraramos@gmail.com>
2021-09-03 17:57:00 -0300

    create NotFoundView

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomeView from '../../views/HomeView';
+import NotFoundView from '../../views/NotFoundView';
 import './App.css'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={HomeView} />
+          <Route component={NotFoundView} />
         </Switch>
       </BrowserRouter>
     </div>
